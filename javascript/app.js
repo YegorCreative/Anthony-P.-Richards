6da3c501465d8217f46b664e813e9a154782f5af
GrigorNurijanyan <grigornurijanyan@gmail.com>
2021-01-21 01:41:02 +0400

    All

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.past-img').style.display = 'none';
+setTimeout(function () {
+    document.querySelector('.past-img').style.display = 'block';
+}, 2900);
+
+document.querySelector('.past-name').style.display = 'none';
+setTimeout(function () {
+    document.querySelector('.past-name').style.display = 'block';
+}, 2900);
