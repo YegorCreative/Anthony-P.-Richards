fa71ea39cc59eeedbb9abb5e775c1253a28dca37
GrigorNurijanyan <grigornurijanyan@gmail.com>
2021-01-22 03:13:52 +0400

    All

--- javascript/app.js
+++ javascript/app.js
@@ -2,9 +2,24 @@
 document.querySelector('.past-img').style.display = 'none';
 setTimeout(function () {
     document.querySelector('.past-img').style.display = 'block';
-}, 2900);
+}, 3100);
 
 document.querySelector('.past-name').style.display = 'none';
 setTimeout(function () {
     document.querySelector('.past-name').style.display = 'block';
-}, 2900);
+}, 2500);
+
+document.querySelector('.Sydney').style.display = 'none';
+setTimeout(function () {
+    document.querySelector('.Sydney').style.display = 'block';
+}, 3800);
+
+document.querySelector('.leftSideh1').style.display = 'none';
+setTimeout(function () {
+    document.querySelector('.leftSideh1').style.display = 'block';
+}, 5000);
+
+document.querySelector('.slight').style.display = 'none';
+setTimeout(function () {
+    document.querySelector('.slight').style.display = 'block';
+}, 6000);
