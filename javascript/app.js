cd7589c78a9e2223c4fc13f363279284cd51b974
GrigorNurijanyan <grigornurijanyan@gmail.com>
2021-01-22 23:18:06 +0400

    All

--- javascript/app.js
+++ javascript/app.js
@@ -9,17 +9,17 @@ setTimeout(function () {
     document.querySelector('.past-name').style.display = 'block';
 }, 2500);
 
-document.querySelector('.Sydney').style.display = 'none';
-setTimeout(function () {
-    document.querySelector('.Sydney').style.display = 'block';
-}, 3800);
+// document.querySelector('.Sydney').style.display = 'none';
+// setTimeout(function () {
+//     document.querySelector('.Sydney').style.display = 'block';
+// }, 3800);
 
-document.querySelector('.leftSideh1').style.display = 'none';
-setTimeout(function () {
-    document.querySelector('.leftSideh1').style.display = 'block';
-}, 5000);
+// document.querySelector('.leftSideh1').style.display = 'none';
+// setTimeout(function () {
+//     document.querySelector('.leftSideh1').style.display = 'block';
+// }, 5000);
 
-document.querySelector('.slight').style.display = 'none';
-setTimeout(function () {
-    document.querySelector('.slight').style.display = 'block';
-}, 6000);
+// document.querySelector('.slight').style.display = 'none';
+// setTimeout(function () {
+//     document.querySelector('.slight').style.display = 'block';
+// }, 6000);
