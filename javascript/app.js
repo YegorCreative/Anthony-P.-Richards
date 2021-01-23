c43b65e129d06756a78815633af2cb8cf27fb406
GrigorNurijanyan <grigornurijanyan@gmail.com>
2021-01-24 01:21:01 +0400

    All

--- javascript/app.js
+++ javascript/app.js
@@ -1,38 +1,50 @@
 
-document.querySelector('.past-img').style.display = 'none';
-setTimeout(function () {
-    document.querySelector('.past-img').style.display = 'block';
-}, 3100);
+// document.querySelector('.past-img').style.display = 'none';
+// setTimeout(function () {
+//     document.querySelector('.past-img').style.display = 'block';
+// }, 3100);
 
-document.querySelector('.past-name').style.display = 'none';
-setTimeout(function () {
-    document.querySelector('.past-name').style.display = 'block';
-}, 2500);
+// document.querySelector('.past-name').style.display = 'none';
+// setTimeout(function () {
+//     document.querySelector('.past-name').style.display = 'block';
+// }, 2500);
 
-$(window).scroll(function () {
-    $('.animation-right').each(function () {
-        var imagePos = $(this).offset().top;
+// $(window).scroll(function () {
+//     $('.animation-right').each(function () {
+//         var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 450) {
-            $(this).addClass("slideRight");
-        }
-    });
-});
+//         var topOfWindow = $(window).scrollTop();
+//         if (imagePos < topOfWindow + 450) {
+//             $(this).addClass("slideRight");
+//         }
+//     });
+// });
 
-$(window).scroll(function () {
-    $('.animation-left').each(function () {
-        var imagePos = $(this).offset().top;
+// $(window).scroll(function () {
+//     $('.animation-left').each(function () {
+//         var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow + 450) {
-            $(this).addClass("slideLeft");
-        }
-    });
-});
+//         var topOfWindow = $(window).scrollTop();
+//         if (imagePos < topOfWindow + 450) {
+//             $(this).addClass("slideLeft");
+//         }
+//     });
+// });
+
+// $(window).scroll(function () {
+//     $('.hhhh').each(function () {
+//         var imagePos = $(this).offset().top;
+
+//         var topOfWindow = $(window).scrollTop();
+//         if (imagePos < topOfWindow + 450) {
+//             $(this).addClass("aos-init");
+//             $(this).attr("data-aos","fade-down");
+//         }
+//     });
+// });
 
 
-$('.element-to-hide').css('visibility', 'hidden');
+// $('.element-to-hide').css('visibility', 'hidden');
 
 // document.querySelector('.Sydney').style.display = 'none';
 // setTimeout(function () {
